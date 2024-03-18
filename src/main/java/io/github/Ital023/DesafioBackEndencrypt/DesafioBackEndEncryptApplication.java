3af7909c71fe718a6a9e4712550d7179aa039c01
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-18 16:29:27 -0300

    feat: initial files

---
+++ src/main/java/io/github/Ital023/DesafioBackEndencrypt/DesafioBackEndEncryptApplication.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.DesafioBackEndencrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackEndEncryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackEndEncryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Ital023/DesafioBackEndencrypt/DesafioBackEndEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.DesafioBackEndencrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndEncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
